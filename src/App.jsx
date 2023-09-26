b7023c18328d7f8d0e230d37f02c87930a8ba346
Ale Suzuki <49762170+alesuzuki@users.noreply.github.com>
2023-09-26 00:57:41 +0000

    cards virando dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,35 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+
+  const item1 = {
+    nome: 'Patrô',
+    imageUrl: 'https://cdn.discordapp.com/attachments/1108770471232684153/1152000204879433728/1.png'
+  }
+
+  const item2 = {
+    nome: 'Stalkin',
+    imageUrl: 'https://cdn.discordapp.com/attachments/1108770471232684153/1152000205542150205/2.png'
+  }
+
   return (
     <>
-      <Card/>
+    {/* <Card item="teste"/>
+    coloco chaves pq tudo que é javascript vai dentro de chaves
+    uma string */}
 
-      
+    <Card item={item1} />
 
-      <br></br>
+    <Card item={item2} />
 
+    {/*
       <div class="card">
         <h2>Patro</h2>
         <h2>o virus que pede sub</h2>
         <img src="https://cdn.discordapp.com/attachments/1108770471232684153/1152000204879433728/1.png" />
       </div>
+      */}
+
     </> 
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,13 @@
 import "./Card.css"
 
 //declarando componente
-export default function Card() {
+export default function Card(props) {
+    // console.log(props);
+    const item = props.item;
+
     return <div class="card">
-    <h2>TABERNA 404</h2>
+    {/* <h2>TABERNA 404</h2> */}
+    <h2>{item.nome}</h2>
 
     <div className="tags">
       <span className="tag">Status: Off-line</span>
@@ -11,6 +15,9 @@ export default function Card() {
       <span className="tag">Pais: BR</span>
     </div>
 
-    <img src="https://cdn.discordapp.com/attachments/1108770471232684153/1152239724036116500/Logotipo_podcast_ilustrado_neon_amarelo_1.png" />
+    {/* <img src="https://cdn.discordapp.com/attachments/1108770471232684153/1152239724036116500/Logotipo_podcast_ilustrado_neon_amarelo_1.png" />
+   */}
+<img src= {item.imageUrl}/>
+   
   </div>
 }
