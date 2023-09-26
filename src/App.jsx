92776565e8dcd2445576f6156221f12fe33cf874
Ale Suzuki <49762170+alesuzuki@users.noreply.github.com>
2023-09-26 01:34:51 +0000

    lista e cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const item1 = {
     nome: 'Patrô',
     imageUrl: 'https://cdn.discordapp.com/attachments/1108770471232684153/1152000204879433728/1.png'
+    tag: 
   }
 
   const item2 = {
@@ -13,15 +14,24 @@ function App() {
     imageUrl: 'https://cdn.discordapp.com/attachments/1108770471232684153/1152000205542150205/2.png'
   }
 
+  const item3 = {
+    nome: 'Kratos',
+    imageUrl: 'https://cdn.discordapp.com/attachments/1108770471232684153/1151593517945213018/kratos-numa-harley-contra-arranhadores-de-moto.png'
+  }
+
+
+  const itens = [item1, item2, item3]
+
+
   return (
     <>
     {/* <Card item="teste"/>
     coloco chaves pq tudo que é javascript vai dentro de chaves
     uma string */}
 
-    <Card item={item1} />
-
+    {/*<Card item={item1} />
     <Card item={item2} />
+*/}
 
     {/*
       <div class="card">
@@ -30,6 +40,19 @@ function App() {
         <img src="https://cdn.discordapp.com/attachments/1108770471232684153/1152000204879433728/1.png" />
       </div>
       */}
+
+    {/*
+      {itens.map(function (item) {
+        return <div>Item</div>
+      })}
+
+      */}
+
+
+
+      {itens.map(function (parametrodafunction) {
+        return <Card item={parametrodafunction}/>
+      })}
 
     </> 
   )
